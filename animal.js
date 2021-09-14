c4adfe20fcd4a0094e205f6197b3e7ac41a190bf
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-09-14 10:27:44 +0700

    update

--- animal.js
+++ animal.js
@@ -1,29 +1,29 @@
 class Animal{
     constructor(_name,_weight) {
-        this.name=_name
-        this.weight=_weight
+        this.name=_name;
+        this.weight=_weight;
     }
     setName(name){
-        this.name=name
+        this.name=name;
     }
     setWeight(weight){
-        this.weight=weight
+        this.weight=weight;
     }
     getName(){
-        return this.name
+        return this.name;
     }
     getWeight(){
-        return this.weight
+        return this.weight;
     }
     toString(){
         return ('Name: '+this.getName()+' Weight:'+this.getWeight())
     }
 }
-let objAnimal1= new Animal()
-objAnimal1.setName('Elephant')
-objAnimal1.setWeight(45.6)
+let objAnimal1= new Animal();
+objAnimal1.setName('Elephant');
+objAnimal1.setWeight(45.6);
 objAnimal1.toString();
-console.log(objAnimal1.toString())
+console.log(objAnimal1.toString());
 let obkAnimal2=new Animal('Dragon',15);
-obkAnimal2.setName('Mouse')
-console.log(obkAnimal2)
+obkAnimal2.setName('Mouse');
+console.log(obkAnimal2);
